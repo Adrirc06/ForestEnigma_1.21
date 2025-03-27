cf5f6b301f9a6d4a95d6ccd88a94450dcde2fefb
Adrián Rosa <adrian.roscue@educa.jcyl.es>
2025-03-27 23:01:12 +0100

    El primer madero no funciona

--- src/main/java/es/elite/forestenigma/block/ModBlocks.java
+++ src/main/java/es/elite/forestenigma/block/ModBlocks.java
@@ -24,6 +24,10 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
+    public static final RegistryObject<Block> MOSSY_WOOD_BLOCK = registerBlock("mossy_wood_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).sound(SoundType.WOOD)));
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
