2ff1a339e7ace10d07f2cd01f1f772e6370d4965
Adrián Rosa <adrian.roscue@educa.jcyl.es>
2025-05-10 18:09:58 +0200

    Completado el setup de la generacion de mundo

---
+++ src/main/java/es/elite/forestenigma/datagen/ModDatapackEntries.java
@@ -0,0 +1,28 @@
+package es.elite.forestenigma.datagen;
+
+import es.elite.forestenigma.ForestEnigma;
+import es.elite.forestenigma.worldgen.ModBiomeModifier;
+import es.elite.forestenigma.worldgen.ModConfiguredFeatures;
+import es.elite.forestenigma.worldgen.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
+
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifier::bootstrap);
+
+    public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(ForestEnigma.MOD_ID));
+    }
+
+}
---
+++ src/main/java/es/elite/forestenigma/worldgen/ModBiomeModifier.java
@@ -0,0 +1,27 @@
+package es.elite.forestenigma.worldgen;
+
+import es.elite.forestenigma.ForestEnigma;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBiomeModifier {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeature = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(ForestEnigma.MOD_ID, name));
+    }
+
+}
