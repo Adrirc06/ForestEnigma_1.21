dbeb4ed62bb19c101d460054b8a701a411eb8e6a
Sampe <diegosamgar@gmail.com>
2025-05-11 20:26:23 +0200

    Generación natural de arboles añadida

--- src/main/java/es/elite/forestenigma/worldgen/ModBiomeModifier.java
+++ src/main/java/es/elite/forestenigma/worldgen/ModBiomeModifier.java
@@ -7,6 +7,7 @@ import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
@@ -15,6 +16,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModBiomeModifier {
 
     public static final ResourceKey<BiomeModifier> ADD_SYLVANITE_ORE = registerKey("add_sylvanite_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_MOSSY_TREE = registerKey("add_mossy_tree");
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         var placedFeature = context.lookup(Registries.PLACED_FEATURE);
@@ -26,6 +29,11 @@ public class ModBiomeModifier {
                 GenerationStep.Decoration.UNDERGROUND_ORES
         ));
 
+        context.register(ADD_MOSSY_TREE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.PLAINS)),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacedFeatures.MOSSY_TREE_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION
+        ));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/es/elite/forestenigma/worldgen/ModPlacedFeatures.java
+++ src/main/java/es/elite/forestenigma/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package es.elite.forestenigma.worldgen;
 
 import es.elite.forestenigma.ForestEnigma;
+import es.elite.forestenigma.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,12 +22,18 @@ public class ModPlacedFeatures {
 
     public static final ResourceKey<PlacedFeature> SYLVANITE_ORE_PLACED_KEY = registerKey("sylvanite_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> MOSSY_TREE_PLACED_KEY = registerKey("mossy_tree_placed");
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
         register(context, SYLVANITE_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.SYLVANITE_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(7,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
+
+        register(context, MOSSY_TREE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.MOSSY_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(20,0.1f,5),
+                        ModBlocks.MOSSY_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
