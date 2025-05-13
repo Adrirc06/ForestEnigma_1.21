a79b1f067a2ffa151c841239e3ad47adfffb6a41
ruben <rubenmail2016@gmail.com>
2025-05-14 00:18:30 +0200

    sounds

--- src/main/java/es/elite/forestenigma/villages/ModVillagers.java
+++ src/main/java/es/elite/forestenigma/villages/ModVillagers.java
@@ -3,12 +3,19 @@ package es.elite.forestenigma.villages;
 import com.google.common.collect.ImmutableSet;
 import es.elite.forestenigma.ForestEnigma;
 import es.elite.forestenigma.block.ModBlocks;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import javax.annotation.Nullable;
 
 
 public class ModVillagers {
@@ -20,15 +27,37 @@ public class ModVillagers {
 
 
     public static final RegistryObject<PoiType> ForestEnigma_POI = POI_TYPES.register("forestEnigma_poi",
-            () -> new PoiType(ImmutableSet.copyOf(ModBlocks.CARITA_BLOCK.get().getStateDefinition().getPossibleStates()),
+            () -> new PoiType(ImmutableSet.copyOf(Blocks.COMPOSTER.getStateDefinition().getPossibleStates()),
                     1, 1));
 
+    public static final RegistryObject<VillagerProfession> KAUPENGER = VILLAGER_PROFESSIONS.register("kaupenger",
+            () -> new VillagerProfession("kaupenger", holder -> holder.value() == ForestEnigma_POI.get(),
+                    holder -> holder.value() == ForestEnigma_POI.get(), ImmutableSet.of(), ImmutableSet.of(),
+                    ModSounds.MAGIC_BLOCK_HIT.get()));
 
 
     public static void register(IEventBus eventBus) {
         POI_TYPES.register(eventBus);
         VILLAGER_PROFESSIONS.register(eventBus);
     }
+    /* SOUNDS */
+/*
+    @Nullable
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ELDER_GUARDIAN_AMBIENT;
+    }
 
+   @Nullable
+    @Override
+    protected SoundEvent getHurtSound(DamageSource pDamageSource) {
+        return SoundEvents.RAVAGER_HURT;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.SNIFFER_DEATH;
+    }*/
 
 }
